9ad343e9071c10a658d711d0fd7e403a7c73f219
SwethaMenon170104 <150894878+SwethaMenon170104@users.noreply.github.com>
2024-06-22 21:47:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
